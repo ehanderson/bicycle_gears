c8bd4f7c33abdcee16532030b92d835e912495a7
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 14:28:00 -0500

    lovin me some pizza bike

--- bike_spec.rb
+++ bike_spec.rb
@@ -4,4 +4,28 @@ require_relative 'bike'
 
 describe Pizza do
   let(:pizza) { Pizza.new(desc) }
+
+
+  describe "#initialize" do
+    it "takes a name for its first argument" do
+      pizza.should be_an_instance_of Pizza
+    end
+
+    it "requires one argument" do
+      expect { Pizza.new }.to raise_error(ArgumentError)
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
